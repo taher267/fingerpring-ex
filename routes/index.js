094b4bf901f3979fb796ea12f924c868f668902f
ABU TAHER <abutaher267@gmail.com>
2023-06-01 22:47:58 +0600

    fjsdkfjsdak

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
-router.use('/auth', require('./authRoute'));
-router.use('/user', require('./userRoute'));
-router.use('/deal', require('./dealRoute'));
-router.use('/stripe-connect', require('./stripeConnectRoute'));
-router.use('/plan', require('./planRoute'));
-router.use('/ltplans', require('./lifetimePlanRoute'));
-router.use('/stripe', require('./stripeRoute'));
-router.use('/subscription', require('./subscriptionRoute'));
+// router.use('/auth', require('./authRoute'));
+// router.use('/user', require('./userRoute'));
+// router.use('/deal', require('./dealRoute'));
+// router.use('/stripe-connect', require('./stripeConnectRoute'));
+// router.use('/plan', require('./planRoute'));
+// router.use('/ltplans', require('./lifetimePlanRoute'));
+// router.use('/stripe', require('./stripeRoute'));
+// router.use('/subscription', require('./subscriptionRoute'));
 
 module.exports = router;
