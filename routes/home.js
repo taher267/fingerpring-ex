90696b4d9bf4c747e9a68e5c8275c59898ba6c50
ABU TAHER <abutaher267@gmail.com>
2023-06-04 09:12:06 +0600

    fsfds

--- routes/home.js
+++ routes/home.js
@@ -2,14 +2,9 @@ const router = require("express").Router();
 router
   .get("/", (req, res) => {
     try {
-      const str = mongoDBURI
-        ?.replace?.("mongodb+srv://", "")
-        ?.split?.("/")?.[1];
+
       res.send({
-        mail: process.env.ALLOW_PAYMENT_MAIL,
-        trail: ALLOW_TRAIL,
-        runningOn: process.env.NODE_ENV,
-        str,
+        mail: 'process.env.ALLOW_PAYMENT_MAIL',
       });
     } catch (e) {
       res.send({ e: e });
